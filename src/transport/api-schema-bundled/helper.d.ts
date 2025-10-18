e17b888e0b7d879199609855bf376afbe6d2d93f
Leon Müller <mueller4leon@gmail.com>
2025-10-18 21:36:11 +0200

    linter

--- src/transport/api-schema-bundled/helper.d.ts
+++ src/transport/api-schema-bundled/helper.d.ts
@@ -1,15 +1,14 @@
-declare function parseStatus_columnJson(column: {
-    columnJSON: string;
-}): {
-    labels: {
-        label: string;
-        backgroundColor: string;
-        enumKey: string;
-    }[];
-};
+/* eslint-disable @typescript-eslint/no-unused-vars */
+declare function parseStatus_columnJson(column: { columnJSON: string }): {
+	labels: {
+		label: string
+		backgroundColor: string
+		enumKey: string
+	}[]
+}
 
 declare const helper: {
-    readonly parseStatus_columnJson: typeof parseStatus_columnJson;
-};
+	readonly parseStatus_columnJson: typeof parseStatus_columnJson
+}
 
-export { helper };
+export { helper }
