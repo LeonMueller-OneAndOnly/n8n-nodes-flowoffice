79c5675ce54759483efa396422b02a043325ccd0
Leon Müller <mueller4leon@gmail.com>
2025-10-19 20:34:25 +0200

    api-upgrade

--- src/transport/api-schema-bundled/api.ts
+++ src/transport/api-schema-bundled/api.ts
@@ -1,101 +1,119 @@
 import { z } from 'zod';
 
 const ZColumnType$1 = z.enum([
-    //mandatory columns
-    "name",
-    // regular columns
-    "text",
-    "number",
-    "date",
-    "checkbox",
-    "interval",
-    "phone",
-    "email",
-    "address",
-    "rating-stars",
-    "erneut-kontaktieren",
-    "link",
-    "personName",
-    "zeitauswertung",
-    "formel",
-    // columns with options
-    "status",
-    "dokument",
-    // project special columns
-    "kunde",
-    "teamMember",
-    "aufgaben",
-    "cloud",
-    "lager",
+  //mandatory columns
+  "name",
+
+  // regular columns
+  "text",
+  "number",
+  "date",
+  "checkbox",
+  "interval",
+
+  "phone",
+  "email",
+  "address",
+
+  "rating-stars",
+  "erneut-kontaktieren",
+  "link",
+  "personName",
+
+  "zeitauswertung",
+
+  "formel",
+
+  // columns with options
+  "status",
+  "dokument",
+
+  // project special columns
+  "kunde",
+  "teamMember",
+  "aufgaben",
+  "cloud",
+  "lager",
 ]);
+
 const ZColumn = z.object({
-    columnKey: z.string(),
-    label: z.string(),
-    columnType: ZColumnType$1,
-    columnJSON: z.string().optional(), // For additional column options
-    deactivated: z.boolean().optional(), // Instead of deleting columns only deactivate them to prevent users from shotting themself in the leg
-    disableEditing: z.boolean().optional(), // For showing rows, but preventing users from editing them
+  columnKey: z.string(),
+  label: z.string(),
+  columnType: ZColumnType$1,
+
+  columnJSON: z.string().optional(), // For additional column options
+  deactivated: z.boolean().optional(), // Instead of deleting columns only deactivate them to prevent users from shotting themself in the leg
+  disableEditing: z.boolean().optional(), // For showing rows, but preventing users from editing them
 });
+
 const ZBoard = z.object({
-    boardId: z.number().int(),
-    name: z.string(),
-    columnSchema: ZColumn.array(),
-    subboards: z
-        .object({
-        name: z.string(),
-        subboardId: z.number().int(),
+  boardId: z.number().int(),
+  name: z.string(),
+  columnSchema: ZColumn.array(),
+  subboards: z
+    .object({
+      name: z.string(),
+      subboardId: z.number().int(),
     })
-        .array(),
+    .array(),
 });
+
 const ZListBoardsOutput = z.object({
-    boardGroups: z
-        .object({
-        groupName: z.string(),
-        boards: z
-            .discriminatedUnion("type", [
-            z.object({
-                type: z.literal("board"),
-                board: ZBoard,
-            }),
-            z.object({
-                type: z.literal("group"),
-                groupId: z.string(),
-                groupName: z.string(),
-                boards: z.array(ZBoard),
-            }),
+  boardGroups: z
+    .object({
+      groupName: z.string(),
+      boards: z
+        .discriminatedUnion("type", [
+          z.object({
+            type: z.literal("board"),
+            board: ZBoard,
+          }),
+          z.object({
+            type: z.literal("group"),
+            groupId: z.string(),
+            groupName: z.string(),
+            boards: z.array(ZBoard),
+          }),
         ])
-            .array(),
-    })
         .array(),
+    })
+    .array(),
 });
 
 const ZColumnType = z.enum([
-    //mandatory columns
-    "name",
-    // regular columns
-    "text",
-    "number",
-    "date",
-    "checkbox",
-    "interval",
-    "phone",
-    "email",
-    "address",
-    "rating-stars",
-    "erneut-kontaktieren",
-    "link",
-    "personName",
-    "zeitauswertung",
-    "formel",
-    // columns with options
-    "status",
-    "dokument",
-    // project special columns
-    "kunde",
-    "teamMember",
-    "aufgaben",
-    "cloud",
-    "lager",
+  //mandatory columns
+  "name",
+
+  // regular columns
+  "text",
+  "number",
+  "date",
+  "checkbox",
+  "interval",
+
+  "phone",
+  "email",
+  "address",
+
+  "rating-stars",
+  "erneut-kontaktieren",
+  "link",
+  "personName",
+
+  "zeitauswertung",
+
+  "formel",
+
+  // columns with options
+  "status",
+  "dokument",
+
+  // project special columns
+  "kunde",
+  "teamMember",
+  "aufgaben",
+  "cloud",
+  "lager",
 ]);
 
 const ZCreateProjectsInput = z.object({
@@ -137,7 +155,7 @@ const ZGetProjectsInput = z.object({
     skip: z.number().int().min(0).optional(),
 });
 const ZGetProjectsOutput = z.object({
-    projekte: z
+    projects: z
         .object({
         dbId: z.number().int(),
         uuid: z.string(),
@@ -158,33 +176,35 @@ const ZGetProjectsOutput = z.object({
 });
 
 const n8nApi_v1 = {
-    endpoints: {
-        board: {
-            listBoards: {
-                method: "GET",
-                pathname: "/api/v1/board/list-boards",
-                inputSchema: z.null(),
-                outputSchema: ZListBoardsOutput,
-            },
-        },
-        project: {
-            createProjects: {
-                method: "POST",
-                pathname: "/api/v1/project/create-projects",
-                inputSchema: ZCreateProjectsInput,
-                outputSchema: ZCreateProjectsOutput,
-            },
-            getProjects: {
-                method: "POST",
-                pathname: "/api/v1/project/get-projects",
-                inputSchema: ZGetProjectsInput,
-                outputSchema: ZGetProjectsOutput,
-            },
-        },
+  endpoints: {
+    board: {
+      listBoards: {
+        method: "GET",
+        pathname: "/api/v1/board/list-boards",
+        inputSchema: z.null(),
+        outputSchema: ZListBoardsOutput,
+      },
     },
-    schemas: {
-        ZColumnType: ZColumnType,
+
+    project: {
+      createProjects: {
+        method: "POST",
+        pathname: "/api/v1/project/create-projects",
+        inputSchema: ZCreateProjectsInput,
+        outputSchema: ZCreateProjectsOutput,
+      },
+
+      getProjects: {
+        method: "POST",
+        pathname: "/api/v1/project/get-projects",
+        inputSchema: ZGetProjectsInput,
+        outputSchema: ZGetProjectsOutput,
+      },
     },
+  },
+  schemas: {
+    ZColumnType: ZColumnType,
+  },
 };
 
 export { n8nApi_v1 };
